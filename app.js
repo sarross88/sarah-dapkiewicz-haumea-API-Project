d2ec72df56b3f55487d9d0793a65d28c20d0b413
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-05-07 21:15:58 -0400

    made API calls to fetch images

--- app.js
+++ app.js
@@ -14,65 +14,72 @@ https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&
 
 /*THE RED ARMCHAIR ID  5357*/
 
-/*THE SEATED NUDE ID 7124
+/*THE SEATED NUDE ID 7124*/
 
 
-https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg*/
 
-const testImg = document.getElementById('test');
-
-console.log(testImg.src);
-
-async function getArtById(id) {
+async function getArtById(imageNum, id, artist) {
     try {    
-      const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
+      const response = await fetch(`https://api.artic.edu/api/v1/artworks/${imageNum}`);
       const data = await response.json();
       if (!response.ok) {
         throw new Error(response.status);  
       }
       console.log(data);
       console.log(data.data);
-    const url = data.data.image_url;
+    const url = data.data.image_id;
    const date = data.data.date_end;
   const artistTitle = data.data.artist_title;
   console.log(artistTitle);
   console.log(date);
   console.log(url);
- 
   
+  const imgUrl = `https://www.artic.edu/iiif/2/${url}/full/200,/0/default.jpg`
+  console.log(imgUrl);
+  const testImg = document.getElementById(id);
+  console.log(testImg);
+  testImg.src = imgUrl;
+  const imgArtist = document.getElementById(artist);
+  imgArtist.innerHTML = artistTitle;
     } catch (error) {
       alert('Could not fetch user, try resetting your connection');
       console.error(error);  
     } 
   }
-  getArtById(5357);
 
-  imgSrc ()
+  getArtById(5357, 'one', "artwork-name-one");
+  getArtById(7124, 'two', "artwork-name-two");
+  getArtById(869, 'three', "artwork-name-three");
+  getArtById(8958, "artwork-name-three");
 
-  async function imgSrc () {
+
+
+async function getBanner(imageNum, id) {
     try {    
-      const response = await fetch('https://www.artic.edu/iiif/2/7021/full/843,/0/default.jpg');
+      const response = await fetch(`https://api.artic.edu/api/v1/artworks/${imageNum}`);
       const data = await response.json();
       if (!response.ok) {
         throw new Error(response.status);  
       }
       console.log(data);
-  
+      console.log(data.data);
+    const url = data.data.image_id;
+   const date = data.data.date_end;
+  const artistTitle = data.data.artist_title;
+  console.log(artistTitle);
+  console.log(date);
+  console.log(url);
+  const imgUrl = `https://www.artic.edu/iiif/2/${url}/full/843,/0/default.jpg`;
+  console.log(imgUrl);
+  const img = document.getElementById(id);
+  console.log(img);
+  img.src = imgUrl;
     } catch (error) {
       alert('Could not fetch user, try resetting your connection');
       console.error(error);  
     } 
   }
-  imgSrc ()
-
-  
 
 
-  /*  const projectSection = document.getElementById("projects");
-    const projectList = projectSection.querySelector("ul");
-    for (let i = 0; i < data.length; i++) {
-        const project = document.createElement("li");
-        project.innerText = data[i].html_url;
-        projectList.appendChild(project);
-    }
-*/
+  getBanner(4758, "banner-image");
+
